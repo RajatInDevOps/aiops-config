1b6d28ed15efca29ca0b7fc92886fc72ec1949c6
RajatInDevOps <rajatsharma@gmail.com>
2026-04-28 04:29:17 +0000

    wrong config

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ module.exports = {
     PORT: 3000,
 
     API: {
-        BASE_URL: "https://jsonplaceholder.typicode.com",
+        BASE_URL: "https://https://wrong-api-url.com",
         TIMEOUT: 2000
     }
 };
